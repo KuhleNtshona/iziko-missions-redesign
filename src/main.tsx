e28b9392508cbf69576ad5c2b4dd602ce49c0d4a
KuhleNtshona <kurra.ntshona@gmail.com>
2026-09-08 20:12:48 +0200

    Add shared page routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from '../app/page';
+import AboutPage from '../app/about/page';
+import TeamPage from '../app/team/page';
+import GetInvolved from '../app/get-involved/page';
 import '../app/globals.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><Home /></React.StrictMode>);
+const path = window.location.pathname.replace(/\/+$/, '');
+const Page = path.endsWith('/about') ? AboutPage : path.endsWith('/team') ? TeamPage : path.endsWith('/get-involved') ? GetInvolved : Home;
+
+createRoot(document.getElementById('root')!).render(<React.StrictMode><Page /></React.StrictMode>);
